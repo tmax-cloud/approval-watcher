d12435d437dbc7ecf84443490ce82501edb843c8
Sunghyun Kim <cqbqdd11519@gmail.com>
2020-07-09 09:54:44 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tmax-cloud/approval-watcher
+
+go 1.13
+
+require (
+	k8s.io/apimachinery v0.17.6
+	sigs.k8s.io/controller-runtime v0.5.2
+)
---
+++ pkg/apis/tmax/v1/approval_types.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ApprovalSpec defines the desired state of Approval
+type ApprovalSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// ApprovalStatus defines the observed state of Approval
+type ApprovalStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Approval is the Schema for the approvals API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=approvals,scope=Namespaced
+type Approval struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ApprovalSpec   `json:"spec,omitempty"`
+	Status ApprovalStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ApprovalList contains a list of Approval
+type ApprovalList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Approval `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Approval{}, &ApprovalList{})
+}
