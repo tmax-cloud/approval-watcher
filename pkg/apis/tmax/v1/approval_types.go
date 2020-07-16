c497f491437c73de7ea74889290f3ce9aafc0024
Sunghyun Kim <cqbqdd11519@gmail.com>
2020-07-16 10:26:53 +0900

    [feat] Add status column for Approval CR
    Add Status column to show if the Approval is approved or not, when user
    executes 'kubectl get approvals'

--- pkg/apis/tmax/v1/approval_types.go
+++ pkg/apis/tmax/v1/approval_types.go
@@ -43,6 +43,7 @@ type ApprovalStatus struct {
 // Approval is the Schema for the approvals API
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=approvals,scope=Namespaced
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.result",description="Current status of Approval"
 type Approval struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
